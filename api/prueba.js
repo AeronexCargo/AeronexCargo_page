a6fb56a18fdd9fac496fffd04424c2a96b48431d
Esteban Giraldo Jaramillo <giraldoesteban044@gmail.com>
2023-09-15 14:37:20 -0500

    ajuste

--- api/prueba.js
+++ api/prueba.js
@@ -42,55 +42,58 @@ function execute(numberAwb) {
         });
       })
       .then(data => {
-
+        const principal = document.createElement('DIV')
+        principal.classList.add('row')
         const contenido = document.createElement('DIV') 
-        const contenido2 = document.createElement('DIV')          
+        contenido.classList.add('col', 'align-bottom')
+        // contenido.classList.add('row')
+        const contenido2 = document.createElement('DIV') 
+        // contenido2.classList.add('col','routeBox')         
+        contenido2.classList.add('col')         
+               
         const segment = data.airwaybill.routingSegments[0]
         const events = Object.values(data.airwaybill.events)          
         
         contenido.innerHTML = `
-            <h4>Summary</h4>
+            <h4>Summary</h4><br/>
             <strong>Air Waybill: </strong>${data.airwaybill.airlinePrefix}-${data.airwaybill.serialNumber} <br/>
             <strong>Status: </strong>${data.airwaybill.routingSegments[0].actionStatus.description}<br/>
-            <strong>Shipper:</strong>  <br />
-            <strong>Consignee:</strong> <br />
+            <strong>Shipper:</strong>  <br/>
+            <strong>Consignee:</strong> <br/>
             <strong>Route: </strong>${data.airwaybill.origin.code}-${data.airwaybill.destination.code}<br/>
             <strong>Pieces/Weight/Volume</strong> ${data.airwaybill.pieces}/${data.airwaybill.weight.amount}/ 
                            ${data.airwaybill.volume.amount}<br/>
-            <strong>Service:</strong> <br />
-            <strong>SCC: </strong> <br />
-            <strong>Goods: </strong>${data.airwaybill.natureOfGoods} <br/>             
+            <strong>Service:</strong> <br/>
+            <strong>SCC: </strong> <br/>
+            <strong>Goods: </strong>${data.airwaybill.natureOfGoods}<br/><br/>           
         `
         contenido2.innerHTML = `
-        <div class="container box-route routeBox">
-            <table class="table table-ccm-day">                
-              <thead>
-                  <tr>
-                      <th>Flight</th>
-                      <th>Date</th>
-                      <th>Segment</th>
-                      <th>Planned /Pieces / Weight</th>                    
-                  </tr>  
-                </thead>                
-                <tbody>
-                  <tr>
-                      <td>${segment.transportMeans.carrier.code}-
-                          ${segment.transportMeans.transportNumber}</td>
-                      <td>${segment.transportMeans.date}</td>
-                      <td>${segment.onload.code}-
-                          ${segment.offload.code}</td>
-                      <td>${segment.pieces}/ ${segment.weight.amount}${segment.weight.unit}/ 
-                          ${segment.volume.amount}${segment.volume.unit}</td>                      
-                  </tr> 
-                </tbody>                 
-            </table>
-        </div>
+        
+            <table class="table table-ccm-day">             
+              
+                <tr class="headers">
+                    <th>Flight</th>
+                    <th>Date</th>
+                    <th>Segment</th>
+                    <th>Planned / Pieces / Weight</th>                    
+                </tr>                               
+             
+                <tr>
+                    <td>${segment.transportMeans.carrier.code}-${segment.transportMeans.transportNumber}</td>
+                    <td>${segment.transportMeans.date}</td>
+                    <td>${segment.onload.code}-
+                        ${segment.offload.code}</td>
+                    <td>${segment.pieces} / &nbsp; &nbsp; &nbsp; ${segment.weight.amount}${segment.weight.unit} / 
+                        ${segment.volume.amount}${segment.volume.unit}</td>                      
+                </tr> 
+                              
+            </table>       
           <br/>
           <h4>History</h4> 
         `
         const btn = document.createElement('BUTTON') 
         btn.textContent = 'Show'
-        btn.classList.add('btn-outline-primary')
+        btn.classList.add('active')
         contenido2.appendChild(btn)
 
         btn.addEventListener('click', function(){
@@ -102,8 +105,8 @@ function execute(numberAwb) {
         events.forEach( event => {
 
           const tr = document.createElement('TR')
-          // tr.classList.add('activeField')
-
+          tr.classList.add('headers')
+          
           const time = document.createElement('TD')
           time.innerHTML = `${event.time}`
           tr.append(time)
@@ -126,13 +129,17 @@ function execute(numberAwb) {
           details.innerHTML = ` ${event.transportMeans.reference}`
           tr.append(details)
 
-
           body.append(tr)
           history.append(body)
            
         })        
-          contenido.appendChild(contenido2)
-          container.appendChild(contenido)
+          // contenido.appendChild(contenido2)
+          // container.appendChild(contenido)
+          // contenido.append(contenido2)
+          // principal.appendChild(contenido)
+          principal.append(contenido)
+          principal.append(contenido2)
+          container.appendChild(principal)
             
       })
       .catch(err => {
